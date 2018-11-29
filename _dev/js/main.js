092a1939536812c0fe7a7418126f73350a3fff2b
ponomarev-iv <ponomarev-iv@galagodigital.ru>
2018-11-29 19:24:18 +0500

    update gulpfile.js + update node and npm + update gulp for v.4

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
    //запуск функций
-});
+  });
--- gulpfile.js
+++ gulpfile.js
@@ -1,119 +1,135 @@
 'use strict';
 
-const   gulp = require('gulp'),
-        watch = require('gulp-watch'),
-        prefixer = require('gulp-autoprefixer'),
-        uglify = require('gulp-uglify'),
-        sass = require('gulp-sass'),
-        csso = require('gulp-csso'),
-        imagemin = require('gulp-imagemin'),
-        newer = require('gulp-newer'),
-        browserSync = require("browser-sync"),
-        pug = require('gulp-pug'),
-        reload = browserSync.reload;
+const gulp = require('gulp'),
+  prefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  csso = require('gulp-csso'),
+  imagemin = require('gulp-imagemin'),
+  newer = require('gulp-newer'),
+  size = require('gulp-size'),
+  sourcemaps = require('gulp-sourcemaps'),
+  browsersync = require("browser-sync").create(),
+  pug = require('gulp-pug');
 
-var path = {
-    build: {
-        css: 'public/css/',
-        img: 'public/img/',
-        js: 'public/js/',
-        base: '',
-        html: 'public/'
-    },
-    src: {
-        style: '_dev/scss/all.scss',
-        scss: '_dev/scss/',
-        img: '_dev/img/*.*',
-        js: '_dev/js/*.js',
-        html: '_dev/*.html',
-        pug: '_dev/tmpl/*.pug'
-    },
-    watch: {
-        style: '_dev/scss/**/*.scss',
-        img: '_dev/img/*.*',
-        js: '_dev/js/*',
-        html: '_dev/*.html',
-        pug: '_dev/tmpl/**/*.pug'
-    },
-    clean: 'public/'
+const path = {
+  build: {
+    css: 'public/css/',
+    img: 'public/img/',
+    js: 'public/js/',
+    base: '',
+    html: 'public/'
+  },
+  src: {
+    style: '_dev/scss/all.scss',
+    scss: '_dev/scss/',
+    img: '_dev/img/*.*',
+    js: '_dev/js/*.js',
+    html: '_dev/*.html',
+    pug: '_dev/tmpl/*.pug'
+  },
+  watch: {
+    style: '_dev/scss/**/*.scss',
+    img: '_dev/img/*.*',
+    js: '_dev/js/*',
+    html: '_dev/*.html',
+    pug: '_dev/tmpl/**/*.pug'
+  },
+  clean: 'public/'
 };
 
-var config = {
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
     server: {
-        baseDir: "public/"
+      baseDir: "public/"
     },
-    tunnel: false,
-    host: 'localhost',
-    port: 9000,
-    logPrefix: "Frontend"
-};
-
-gulp.task('webserver', function () {
-    browserSync(config);
-});
-
-gulp.task('style:build', function () {
-    gulp.src(path.src.style)
-        .pipe(sass({
-            errLogToConsole: true
-        }))
-        .on('error', console.log)
-        .pipe(prefixer('last 4 versions'))
-        .pipe(csso())
-        .pipe(gulp.dest(path.build.css))
-        .pipe(reload({stream: true}));
-});
-
-gulp.task('image:build', function () {
-    gulp.src(path.src.img)
-        .pipe(newer(path.build.img))
-        .pipe(imagemin())
-        .on('error', console.log)
-        .pipe(gulp.dest(path.build.img));
-});
-
-gulp.task('js:build', function () {
-    gulp.src(path.src.js)
-        .pipe(uglify())
-        .pipe(gulp.dest(path.build.js))
-        .pipe(reload({stream: true}));
-});
-
-// обработка ошибки pug
-function swallowError (error) {
-    console.log(error.toString());
-    this.emit('end')
+    port: 9000
+  });
+  done();
 }
 
-gulp.task('html:build', function () {
-    gulp.src(path.src.html)
-        .pipe(gulp.dest(path.build.html));
-    gulp.src(path.src.pug)
-        .pipe(pug())
-        .on('error', swallowError)
-        .pipe(gulp.dest(path.build.html));
-});
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
 
-gulp.task('build', [
-    'html:build',
-    'style:build',
-    'image:build',
-    'js:build'
-]);
+function styles() {
+  return gulp
+    .src(path.src.style)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .on('error', console.log)
+    .pipe(prefixer('last 3 versions'))
+    .pipe(csso())
+    .pipe(sourcemaps.write('.'))
+    .pipe(size())
+    .pipe(gulp.dest(path.build.css))
+    .pipe(browsersync.stream());
+}
 
-gulp.task('watch', function(){
-    watch([path.watch.html, path.watch.pug], function(event, cb) {
-        gulp.start('html:build');
-    });
-    watch([path.watch.style], function(event, cb) {
-        gulp.start('style:build');
-    });
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('image:build');
-    });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
-    });
-});
+function images() {
+  return gulp
+    .src(path.src.img)
+    .pipe(newer(path.build.img))
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ]
+    ))
+    .on('error', console.log)
+    .pipe(gulp.dest(path.build.img));
+}
 
-gulp.task('default', ['build', 'webserver', 'watch']);
+function js() {
+  return gulp
+    .src(path.src.js)
+    .pipe(uglify())
+    .pipe(gulp.dest(path.build.js))
+    .pipe(browsersync.stream());
+}
+
+// pug error
+function swallowError(error) {
+  console.log(error.toString());
+  this.emit('end')
+}
+
+function html() {
+  return gulp
+    .src(path.src.pug)
+    .pipe(pug({
+      pretty: true
+    }))
+    .on('error', swallowError)
+    .pipe(gulp.dest(path.build.html));
+}
+
+function watchFiles(){
+  gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.style], styles);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.src.pug], gulp.series(html, browserSyncReload));
+}
+
+// Tasks
+gulp.task("images", images);
+gulp.task("css", styles);
+gulp.task("js", js);
+gulp.task("html", html);
+
+gulp.task('build', gulp.parallel(styles, images, js, html));
+
+// watch
+gulp.task("watch", gulp.parallel(watchFiles, browserSync));
